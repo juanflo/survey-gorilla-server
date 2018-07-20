a7251fda7987d571203eb281a79ba1d51ef64591
Juan Flores <juan.flores@libertymutual.com>
2018-07-20 10:44:41 -0400

    update db script, setup env file, server updates'

--- scripts/db-setup.sql
+++ scripts/db-setup.sql
@@ -2,12 +2,12 @@ DROP DATABASE IF EXISTS gorilla;
 CREATE DATABASE gorilla;
 USE gorilla;
 
-# Dump of table teams
+# Dump of table team
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `teams`;
+DROP TABLE IF EXISTS `team`;
 
-CREATE TABLE `teams` (
+CREATE TABLE `team` (
   `team_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `team_uuid` char(32) DEFAULT '''',
   `team_name` varchar(200) DEFAULT '''',
@@ -20,12 +20,12 @@ CREATE TABLE `teams` (
 
 
 
-# Dump of table surveys
+# Dump of table survey
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `surveys`;
+DROP TABLE IF EXISTS `survey`;
 
-CREATE TABLE `surveys` (
+CREATE TABLE `survey` (
   `survey_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `survey_uuid` char(32) NOT NULL DEFAULT '''',
   `team_id` int(10) unsigned NOT NULL,
@@ -34,20 +34,21 @@ CREATE TABLE `surveys` (
   UNIQUE KEY `survey_uuid` (`survey_uuid`),
   KEY `team_id` (`team_id`),
   KEY `create_date` (`create_date`),
-  CONSTRAINT `delete_surveys_as_team_removed` FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `delete_survey_as_team_removed` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 
 
-# Dump of table questions
+# Dump of table question
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `questions`;
+DROP TABLE IF EXISTS `question`;
 
-CREATE TABLE `questions` (
+CREATE TABLE `question` (
   `question_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `question_uuid` char(32) NOT NULL DEFAULT '',
   `question_text` text NOT NULL,
+  `question_area` text NOT NULL,
   `create_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`question_id`),
   UNIQUE KEY `question_uuid` (`question_uuid`),
@@ -56,12 +57,12 @@ CREATE TABLE `questions` (
 
 
 
-# Dump of table answers
+# Dump of table answer
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `answers`;
+DROP TABLE IF EXISTS `answer`;
 
-CREATE TABLE `answers` (
+CREATE TABLE `answer` (
   `answer_id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
   `answer_uuid` char(32) NOT NULL DEFAULT '',
   `question_id` bigint(20) NOT NULL,
@@ -73,6 +74,25 @@ CREATE TABLE `answers` (
   KEY `question_id` (`question_id`),
   KEY `survey_id` (`survey_id`),
   KEY `create_date` (`create_date`),
-  CONSTRAINT `delete_answers_as_question_removed` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `delete_answers_as_survey_removed` FOREIGN KEY (`survey_id`) REFERENCES `surveys` (`survey_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `delete_answers_as_question_removed` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `delete_answers_as_survey_removed` FOREIGN KEY (`survey_id`) REFERENCES `survey` (`survey_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Insert survey questions into the question table.
+# ------------------------------------------------------------
+
+INSERT INTO question 
+  (question_uuid, question_text, question_area)
+VALUES
+  ('000001', 'We know exactly why we are here, and we are really excited about it.', 'mission'),
+  ('000002', 'We deliver great stuff! We’re proud of it and our stakeholders are really happy.', 'value'),
+  ('000003', 'We love going to work, and have great fun working together.', 'fun'),
+  ('000004', 'We’re learning lots of interesting stuff all the time!', 'learning'),
+  ('000005', 'Releasing is simple, safe, painless & mostly automated.', 'release'),
+  ('000006', 'Our way of working fits us perfectly.', 'process'),
+  ('000007', 'We’re proud of the quality of our code! It is clean, easy to read, and has great test coverage.', 'quality'),
+  ('000008', 'We get stuff done really quickly.No waiting, no delays.', 'speed'),
+  ('000009', 'We always get great support & help when we ask for it!', 'support'),
+  ('000010', 'We are in control of our destiny! We decide what to build and how to build it.', 'self-organization');
