6baf17fe02bbf7573225ecf1cdc3debbed30ef54
Sinh Nguyen <sinh.nguyen@libertymutual.com%>
2018-07-24 09:22:30 -0400

    Data retrieve/analytic sql

---
+++ scripts/getTrendResultsBySurvey.sql
@@ -0,0 +1,31 @@
+WITH avgTable AS 
+(
+	SELECT
+		ans.survey_id, 
+		ans.question_id,
+		AVG(ans.answer_result) AS avgScore,
+		COUNT(ans.answer_id) AS 'countOfAnswers'
+	FROM answer AS ans
+		INNER JOIN survey sur ON sur.survey_id = ans.survey_id
+		INNER JOIN team tea ON tea.team_id = sur.team_id
+	WHERE
+		tea.team_id = '000001'
+	GROUP BY
+		ans.survey_id, 
+		ans.question_id
+)
+SELECT
+	tea.team_name,
+	sur.survey_name,
+	sur.create_date AS surveyDate,
+	CONCAT(month(sur.create_date), '/', year(sur.create_date)) AS surveyDate2,
+	que.question_text,
+	ans.*
+FROM avgTable ans
+	INNER JOIN question que ON que.question_id = ans.question_id
+	INNER JOIN survey sur ON sur.survey_id = ans.survey_id
+	INNER JOIN team tea ON tea.team_id = sur.team_id
+ORDER BY
+	sur.create_date,
+	ans.survey_id,
+	ans.question_id
